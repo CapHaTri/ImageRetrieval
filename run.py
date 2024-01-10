b6ed093310d655107974d7c725a342a1c1b60e5a
CapHaTri <113035712+CapHaTri@users.noreply.github.com>
2024-01-10 23:29:59 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from Img2Vec import ImageVectorizer
 import pickle
+from sklearn.metrics import f1_score
 import numpy as np
 from PIL import Image
 import streamlit as st
@@ -8,6 +9,8 @@ st.subheader(":green[Lưu ý khi sử dụng: ]")
 st.write("**Đưa vào 1 hình ảnh, hệ thống sẽ đưa ra danh sách các hình ảnh tương tự**")
 st.write("**Hệ thống chỉ hỗ trợ đề xuất ảnh với dữ liệu thuộc Animal(...)**" )
 uploaded_file = st.file_uploader("Chọn hình ảnh", type=['png', 'jpg', 'jpeg', 'jfif'])
+y_true = np.ones(16, dtype=int)
+y_pred = []
 if uploaded_file is not None:
     # Đọc và hiển thị hình ảnh tải lên
     image = Image.open(uploaded_file)
@@ -31,21 +34,40 @@ if uploaded_file is not None:
     # Sắp xếp và lấy ra K vector có khoảng cách ngắn nhất
     K = 16
     ids = np.argsort(distance)[:K]
-
+    threshold = 0.8
     # Lưu 
     nearest_image = [paths[id] for id in ids]
-    folder = 'C:/Users/MSI/OneDrive/CV/'
+    distance_image = [distance[id] for id in ids]
+    folder = 'C:/Users/MSI/OneDrive/CV/ImageRetrieval/'
     st.subheader(":blue[KẾT QUẢ TÌM KIẾM]") 
     col1, col2, col3, col4 = st.columns(4)
     with col1:
         for item in [0,1,2,3]:
             st.image(Image.open(folder + nearest_image[item]))
+            if distance_image[item] < threshold:
+                y_pred.append(1)
+            else:
+                y_pred.append(0)
     with col2:
         for item in [4,5,6,7]:
             st.image(Image.open(folder + nearest_image[item]))
+            if distance_image[item] < threshold:
+                y_pred.append(1)
+            else:
+                y_pred.append(0)
     with col3:
         for item in [8,9,10,11]:
             st.image(Image.open(folder + nearest_image[item]))
+            if distance_image[item] < threshold:
+                y_pred.append(1)
+            else:
+                y_pred.append(0)
     with col4:
         for item in [12,13,14,15]:
             st.image(Image.open(folder + nearest_image[item]))  
+            if distance_image[item] < threshold:
+                y_pred.append(1)
+            else:
+                y_pred.append(0)    
+    f1 = f1_score(y_true, y_pred)
+    st.write("Độ chính xác: ", f1)
